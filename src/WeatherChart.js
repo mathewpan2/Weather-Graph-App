c26e9466f502e13ea328f405ff4e82cfdebe9c0c
mathewpan2 <mathew.pan2@gmail.com>
2023-03-25 02:42:55 -0400

    added background color

--- src/WeatherChart.js
+++ src/WeatherChart.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto';
+import React, { useEffect, useRef } from 'react'
+import Chart from 'chart.js/auto'
 
-function WeatherChart({ weatherData }) {
+function WeatherChart({ weatherData, cityName }) {
   const chartRef = useRef(null);
 
   useEffect(() => {
     if (weatherData) {
-      const temperatures = weatherData.list.map(item => item.main.temp);
-      const timeLabels = weatherData.list.map(item => item.dt_txt);
+      const temperatures = weatherData.list.map(item => item.main.temp)
+      const timeLabels = weatherData.list.map(item => item.dt_txt)
+      const descriptions = weatherData.list.map(item => item.weather[0].description)
 
       const chart = new Chart(chartRef.current, {
         type: 'line',
@@ -17,40 +18,66 @@ function WeatherChart({ weatherData }) {
             label: 'Temperature',
             data: temperatures,
             fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
+            borderColor: '#4bc0c0',
+            backgroundColor: 'transparent',
+            pointBackgroundColor: '#4bc0c0',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: '#4bc0c0',
+            lineTension: 0.1,
+            descriptions: descriptions
           }]
         },
         options: {
           responsive: true,
           plugins: {
             tooltip: {
+              backgroundColor: '#2c3e50',
+              titleFontColor: '#fff',
+              bodyFontColor: '#fff',
               callbacks: {
                 label: function(context) {
-                  var label = context.dataset.label || '';
-                  if (label) {
-                    label += ': ';
-                  }
-                  if (context.parsed.y !== null) {
-                    label += context.parsed.y + '°F';
-                  }
-                  return label;
+                  const temperatureLabel = `Weather: ${context.parsed.y}°F`;
+                  const descriptionLabel = ` with ${context.dataset.descriptions[context.dataIndex]}`;
+                  return temperatureLabel + descriptionLabel;
                 }
               }
             }
+          },
+          scales: {
+            x: {
+              ticks: {
+                fontColor: '#ccc',
+                padding: 10
+              }
+            },
+            y: {
+              ticks: {
+                fontColor: '#ccc',
+                padding: 10
+              },
+              min: Math.min(...temperatures) - 2,
+              max: Math.max(...temperatures) + 2,
+              suggestedMin: Math.min(...temperatures) - 2,
+              suggestedMax: Math.max(...temperatures) + 2
+            }
           }
         }
-      });
+      })
 
       return () => {
         chart.destroy();
-      };
+      }
     }
   }, [weatherData]);
 
   return (
-    <canvas ref={chartRef}></canvas>
-  );
+    <div className="chart-container">
+      <h2 className="chart-title">{cityName}</h2>
+      <canvas ref={chartRef} width="800" height="400"></canvas>
+    </div>
+  )
 }
 
 export default WeatherChart;
+
